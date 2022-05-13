42bfca62419a35a302b1e8f5f965510d1deef886
gamaraina <57658819+gamaraina@users.noreply.github.com>
2022-05-13 17:35:40 +0530

    FrontEnd added

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <section className="glass">
+
+        </section>
+        <div className="circle1"></div>
+        <div className="circle2"></div>
+      </main>
+    </>
+  );
+}
+
+export default App;
